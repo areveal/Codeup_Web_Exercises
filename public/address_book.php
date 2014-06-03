94946bd328f6f20c29c9c225662c4be5350f131e
Cole Reveal <alexi.reveal@gmail.com>
2014-06-03 10:53:01 -0500

    cleaned up csv put file added comments

--- public/address_book.php
+++ public/address_book.php
@@ -1,6 +1,5 @@
 <?
-
-function wrtite_csv($file, $book = '$address_book') {
+function csv_out($book, $file = 'address_book.csv') {
 	//open the file
 	$write = fopen($file, 'w');
 	//write each contact to the file
@@ -10,49 +9,31 @@ function wrtite_csv($file, $book = '$address_book') {
 	//close the handle
 	fclose($write);
 }
-
-// function open_contacts($file = 'address_book.csv') {
-// 	if(is_readable($file) && filesize($file) >0) {
-// 		//open the file
-// 		$read = fopen($file, 'r');
-// 		//read it
-// 		while(!feof($read)) {	
-// 			$list[] = fgetcsv($read);
-// 		}
-// 		//close it
-// 		fclose($read);
-
-
-// 	} else {
-// 		//file must be readable
-// 		$list = [['File is not readable']];
-// 	}
-// 		return $list;
-
-// }
-
-
+//some fixed values for our address book
 $address_book = [
     ['The White House', '1600 Pennsylvania Avenue NW', 'Washington', 'DC', '20500'],
     ['Marvel Comics', 'P.O. Box 1527', 'Long Island City', 'NY', '11101'],
     ['LucasArts', 'P.O. Box 29901', 'San Francisco', 'CA', '94129-0901']
 ];
-
-$address_book[] = $_POST;
-
-
+//arbitrary variable to check if form input was valid
 $isValid = false;
-
+//check to see if all the required fields were filled out
 if(!empty($_POST['name']) && !empty($_POST['address']) && !empty($_POST['city']) && !empty($_POST['state']) && !empty($_POST['zip'])) {
 	$isValid = true;
+	if(!empty($_POST['phone'])) {
+		$new_address = $_POST;
+		$address_book[] = $new_address;
+	} else {
+		//if they didnt put the phone field, get rid of the empty string
+		array_pop($_POST);
+		$new_address = $_POST;
+		$address_book[] = $new_address;
+	}
 }
-
+//write to the address book
 if($isValid) {
-
-	save_contacts('address_book.csv', $address_book);
-	header('address_book.php');
+	csv_out($address_book);
 }
-
 
 ?>
 
@@ -62,15 +43,17 @@ if($isValid) {
 	<title>Address Book Writer</title>
 </head>
 <body>
+
 	<h1>Address Book</h1>
 
-	<table style="border:1px solid black">
+	<!--here is our address book loop -->
+	<table style='border: 1px solid black'>
 		<tr><th>Name</th><th>Address</th><th>City</th><th>State</th><th>Zip</th><th>Phone#</th></tr>
 		<? if(!empty($address_book)) : ?>
 			<? foreach($address_book as $contact) : ?>
 				<tr>
 					<?foreach($contact as $info) : ?>
-						<td><?= $info ?></td>
+						<td style='border: 1px solid black'><?= $info ?></td>
 					<? endforeach; ?>
 				</tr>
 			<? endforeach; ?>
@@ -79,9 +62,12 @@ if($isValid) {
 
 
 	<h3>Add Contacts</h3>
+	
+	<!--only show error message if form input is not valid-->
 	<? if((!$isValid) && !empty($_POST)) : ?>
 		<h3 style="color:red">You must input all required fields. </h3>
 	<? endif; ?>
+	<!--'sticky' form-->
 	<form method="POST">
 		<p>
 			<label for="name">Name:</label>
@@ -105,8 +91,9 @@ if($isValid) {
 		</p>
 		<p>
 			<label for="phone">Phone:</label>
-			<input id="phone" type="text" name="phone">
+			<input id="phone" type="text" name="phone" value="<?= (!$isValid && !empty($_POST['phone'])) ? $_POST['phone'] : '' ?>">
 		</p>
+		
 		<input type='submit' value="Add Contact">
 
 	</form>
