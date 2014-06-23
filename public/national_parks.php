c8f94f313a6e5a8724a1ad499efbeb0b89f965c6
Cole Reveal <alexi.reveal@gmail.com>
2014-06-23 15:26:47 -0500

    dropped database table

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,34 +1,45 @@
 <html>
 <head>
 
-
-	<style>
-		body {
-			background-image: url('../img/park.jpg');
-			background-size: cover;
-			color:#fff;
-		}
-		.glyphicon,.page {
-			font-size: 30px;
-			left:100px;
-			color:#fff;
-		}
-		.page {
-			position: relative;
-		}
-		#head,table {
-			color: #fff;
-		}
-	</style>
-
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
+	<script src="../js/jquery/jquery.js"></script>
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+
+	<style>
+		body {
+			background-color: #A2B2AB;
+		}
+		.glyphicon,.page {
+			font-size: 30px;
+			left:100px;
+			color:#17FF9C;
+		}
+		.page {
+			position: relative;
+		}
+		#head,table {
+			color: #B21B00;
+		}
+
+		div	{
+			position: absolute;
+			left: 380px;
+			top:270px;
+		}
+
+		#picture {
+			height: 400px;
+			width: 600px;
+		}
+	</style>
+
 
 	<title>National Parks Table</title>
 </head>
@@ -43,8 +54,10 @@ $dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'cole', 'passwo
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 if(!empty($_GET)) {
-	$offset = intval($_GET['start']);
+	$page = $_GET['page'];
+	$offset = (intval($page)-1)*4;
 } else {
+	$page = 1;
 	$offset = 0;
 }
 
@@ -65,32 +78,55 @@ function getParks($dbc, $offset) {
 		<th>Area in Acres</th>
 	</tr>
 	<? foreach(getParks($dbc, $offset) as $park) : ?>	
-	<tr>
-		<td>
-			<?= $park['name']?>
-		</td>
-		<td>
-			<?= $park['location']?>
-		</td>
-		<td>
-			<?= $park['date_established']?>
-		</td>
-		<td>
-			<?= $park['area_in_acres']?>
-		</td>
-	</tr>
+		<tr id="<?= $park['area_in_acres']?>">
+			<td>
+				<?= $park['name']?>
+			</td>
+			<td>
+				<?= $park['location']?>
+			</td>
+			<td>
+				<?= $park['date_established']?>
+			</td>
+			<td>
+				<?= $park['area_in_acres']?>
+			</td>
+		</tr>
 	<? endforeach; ?>	
 </table>
-<? if($offset > 0) : ?>
-<a href="?start=<?=$offset-4?>"><span class="glyphicon glyphicon-arrow-left"></span></a>
+
+
+
+<? if($page > 1) : ?>
+<a href="?page=<?=$page-1?>"><span class="glyphicon glyphicon-arrow-left"></span></a>
 <? endif;?>
-<a class ="page" href="?start=0">1</a>
-<a class ="page" href="?start=4">2</a>
-<a class ="page" href="?start=8">3</a>
-<?if($offset < 8) :?>
-	<a href="?start=<?=$offset+4?>"><span class="glyphicon glyphicon-arrow-right"></span></a>
+
+<a class ="page" href="?page=1">1</a>
+<a class ="page" href="?page=2">2</a>
+<a class ="page" href="?page=3">3</a>
+
+<?if($page < 3) :?>
+	<a href="?page=<?=$page+1?>"><span class="glyphicon glyphicon-arrow-right"></span></a>
 <?endif;?>
+
+
+<div id="parks"></div>
 
 
 </body>
 </html>
+
+
+<script>
+	<?foreach (getParks($dbc, $offset) as $park) : ?>
+	$("#<?=$park['area_in_acres']?>").mouseenter(function(){
+		$('#parks').html('<img id="picture" src="../img/<?=$park["area_in_acres"]?>.jpg">');
+	});
+	<?endforeach;?>
+
+	<?foreach (getParks($dbc, $offset) as $park) : ?>
+	$("#<?=$park['area_in_acres']?>").mouseleave(function(){
+		$('#picture').remove();
+	});
+	<?endforeach;?>
+</script>
