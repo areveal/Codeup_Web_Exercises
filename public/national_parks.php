b7fe28058e9c09d4dc38d7bd9334ee5723d62118
Cole Reveal <alexi.reveal@gmail.com>
2014-06-23 11:34:38 -0500

    added pagination into database

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,6 +1,26 @@
 <html>
 <head>
 
+
+	<style>
+		body {
+			background-image: url('../img/park.jpg');
+			background-size: cover;
+			color:#fff;
+		}
+		.glyphicon,.page {
+			font-size: 30px;
+			left:100px;
+			color:#fff;
+		}
+		.page {
+			position: relative;
+		}
+		#head,table {
+			color: #fff;
+		}
+	</style>
+
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
 
@@ -14,19 +34,25 @@
 </head>
 <body>
 
-<h1>National Parks</h1>
+<h1 id="head">National Parks</h1>
 <?
 // Get new instance of PDO object
 $dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'cole', 'password');
 
 // Tell PDO to throw exceptions on error
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+if(!empty($_GET)) {
+	$offset = intval($_GET['start']);
+} else {
+	$offset = 0;
+}
+
+
 //this will get us our data
-function getParks($dbc) {
+function getParks($dbc, $offset) {
     // Bring the $dbc variable into scope somehow
-
-    return $dbc->query('SELECT * FROM parks LIMIT 4 OFFSET 0')->fetchAll(PDO::FETCH_ASSOC);
-
+    return $dbc->query("SELECT * FROM parks LIMIT 4 OFFSET $offset")->fetchAll(PDO::FETCH_ASSOC);
 }
 ?>
 
@@ -38,7 +64,7 @@ function getParks($dbc) {
 		<th>Date Established</th>
 		<th>Area in Acres</th>
 	</tr>
-	<? foreach(getParks($dbc) as $park) : ?>	
+	<? foreach(getParks($dbc, $offset) as $park) : ?>	
 	<tr>
 		<td>
 			<?= $park['name']?>
@@ -55,9 +81,15 @@ function getParks($dbc) {
 	</tr>
 	<? endforeach; ?>	
 </table>
-
-<button>Previous Page</button>
-<button>Next Page</button>
+<? if($offset > 0) : ?>
+<a href="?start=<?=$offset-4?>"><span class="glyphicon glyphicon-arrow-left"></span></a>
+<? endif;?>
+<a class ="page" href="?start=0">1</a>
+<a class ="page" href="?start=4">2</a>
+<a class ="page" href="?start=8">3</a>
+<?if($offset < 8) :?>
+	<a href="?start=<?=$offset+4?>"><span class="glyphicon glyphicon-arrow-right"></span></a>
+<?endif;?>
 
 
 </body>
